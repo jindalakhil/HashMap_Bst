8b12ead6229d04a1507ceaee2f99b65fce612072
jindalakhil <akhiljindal222@gmail.com>
2020-10-19 00:20:07 +0530

    [Akhil]Added: Ability to remove word

--- src/main/java/com/capg/hashmap/MyHashMap.java
+++ src/main/java/com/capg/hashmap/MyHashMap.java
@@ -2,39 +2,38 @@ package com.capg.hashmap;
 import com.capg.linkedlist.*;;
 
 public class MyHashMap<K, V> {
+	MyLinkedList<K> myLinkedList;
 
-	   MyLinkedList<K> myLinkedList;
+	public MyHashMap() {
+		this.myLinkedList = new MyLinkedList<>();
+	}
 
-		public MyHashMap() {
-			this.myLinkedList=new MyLinkedList<>();
+	public V get(K key) {
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
+
+		if (myMapNode == null)
+			return null;
+		else
+			return myMapNode.getValue();
+	}
+
+	public void add(K key, V value) {
+
+		MyMapNode myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
+		if (myMapNode == null) {
+			myMapNode = new MyMapNode(key, value);
+			this.myLinkedList.addAtEnd(myMapNode);
+		} else {
+			myMapNode.setValue(value);
 		}
 
-		public V get(K key) {
-			MyMapNode<K,V> myMapNode=(MyMapNode<K,V>)this.myLinkedList.search(key);
+	}
 
+	public void printHashMap() {
+		this.myLinkedList.printList();
+	}
 
-			if(myMapNode==null)
-				return null;
-			
-			else
-				return myMapNode.getValue();	
-		}
-		public void add(K key,V value) {
-
-			MyMapNode myMapNode=(MyMapNode<K,V>)this.myLinkedList.search(key);
-			if(myMapNode==null) {
-				myMapNode=new MyMapNode(key,value);
-				this.myLinkedList.addAtEnd(myMapNode);
-			}
-			else {
-				myMapNode.setValue(value);
-			}
-
-		}
-		public void printHashMap() {
-			this.myLinkedList.printList();
-		}
-		public int getSize() {
-			return this.myLinkedList.lenList();
-		}
-	} 
+	public int getSize() {
+		return this.myLinkedList.lenList();
+	}
+}
--- src/main/java/com/capg/hashmap/MyLinkedHashMap.java
+++ src/main/java/com/capg/hashmap/MyLinkedHashMap.java
@@ -6,14 +6,14 @@ import com.capg.linkedlist.*;
 public class MyLinkedHashMap<K,V> {
 	private final int numBuckets;
 	ArrayList<MyLinkedList<K>> myBucketArray;
-
+	
 	public MyLinkedHashMap() {
 		this.numBuckets=10;
 		this.myBucketArray=new ArrayList<>(numBuckets);
 		for(int i=0;i<numBuckets;i++)
 			myBucketArray.add(null);
 	}
-
+	
 	public int getBucketIndex(K key) {
 		int hashCode = Math.abs(key.hashCode());
 		int index = hashCode % numBuckets;
@@ -28,8 +28,8 @@ public class MyLinkedHashMap<K,V> {
 		if(myMapNode!=null)
 			return myMapNode.getValue();
 		else return null;
-
-
+		
+		
 	}
 	public void add(K key,V value) {
 		int index=this.getBucketIndex(key);
@@ -39,7 +39,7 @@ public class MyLinkedHashMap<K,V> {
 			MyMapNode myMapNode=new MyMapNode(key,value);
 			myLinkedList.addAtEnd(myMapNode);
 			this.myBucketArray.set(index, myLinkedList);
-
+		
 		}
 		else {
 			MyMapNode myMapNode=(MyMapNode) myLinkedList.search(key);
@@ -50,7 +50,19 @@ public class MyLinkedHashMap<K,V> {
 			else {
 				myMapNode.setValue(value);
 			}
-
+			
 		}
 	}
+	
+	public K remove(K key) {
+		int index=this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList=this.myBucketArray.get(index);
+		if(myLinkedList==null)
+			return null;
+		else {
+			MyMapNode deletedNode=(MyMapNode)myLinkedList.deleteAnode(key);
+			return (K)deletedNode.getKey();
+		}
+	}
+	
 }
